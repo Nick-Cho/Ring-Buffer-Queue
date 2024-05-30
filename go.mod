1ca7038f0a92d7747596ee95002ad4d80e04d13b
Nick Cho <nicholas.cho@hotmail.ca>
2024-05-29 20:04:22 -0400

    Ripping consumer code from kafka

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gokafka
+
+go 1.21.0
+
+require github.com/confluentinc/confluent-kafka-go/v2 v2.4.0 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"io"
 	"log/slog"
 	"net"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 type Message struct {
@@ -53,6 +56,36 @@ func (s *Server) handleConn(conn net.Conn) {
 	fmt.Println("New connection: ", conn.RemoteAddr())
 }
 
+func consumer() {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	c.SubscribeTopics([]string{"myTopic", "fooTopic"}, nil)
+
+	// A signal handler or similar could be used to set this to false to break the loop.
+	run := true
+
+	for run {
+		msg, err := c.ReadMessage(time.Second)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		} else if !err.(kafka.Error).IsTimeout() {
+			// The client will automatically try to recover from all errors.
+			// Timeout is not considered an error because it is raised by
+			// ReadMessage in absence of messages.
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
+
+	c.Close()
+}
 func main() {
 	server := NewServer()
 	server.Listen()
